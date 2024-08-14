71a0d7db7f727a3fcc838e5fe28a8542d763eda5
pedro Pablo Talavera Segura <pedrotalaveras@gmail.com>
2024-08-14 17:27:12 -0600

    acabe promesa

--- Promesas/ejemplo1.js
+++ Promesas/ejemplo1.js
@@ -1,3 +1,24 @@
-const promesa = new Promise((resolve, reject) =>{
-    
-})
+const promesa = new Promise((resolve, reject) => {
+    const number = Math.floor(Math.random() * 10);
+
+    setTimeout(() => {
+        number > 5
+        ? resolve(number)
+        : reject(new Error("Menor a 5"))
+    }, 1000);
+});
+
+// Ejecución de la promesa
+promesa
+.then(
+    //Función que se ejecuta si la promesa se cumple
+    (number) => {
+        console.log(number);
+    }
+)
+    .catch(
+        //Función que se ejecuta si la promesa falla
+        (error) => {
+            console.error(error);
+        }
+    )
