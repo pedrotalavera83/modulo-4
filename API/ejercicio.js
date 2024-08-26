1806548bf1686ad8d6cd19f62260430ab012fb7c
pedro Pablo Talavera Segura <pedrotalaveras@gmail.com>
2024-08-26 13:11:25 -0600

    api rckmorty

--- API/ejercicio.js
+++ API/ejercicio.js
@@ -7,16 +7,61 @@
 
 // Desplegar la información usando los recursos de html y css
 
-function buscaPersonaje(numeroId) {
-    fetch("https://rickandmortyapi.com/api/character/" + numeroId)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("Nombre: " + data.name);
-            console.log("Género: " + data.gender);
-            console.log(`Capítulos en los que aparece, ${data.episode.length}:`);
-            data.episode.forEach((episode) => console.log("- " + episode));
-        })
-        .catch((error) => console.error("Error:", error));
+
+//Selecciono los elementos del DOM que voy a utilizar
+const searchButton = document.getElementById("searchButton");
+const rickMortyNameInput = document.getElementById("personajeName");
+const rickMortyResults = document.getElementById("results");
+
+function displayRickMortyResults(results) {
+    // Limpiar el contenido previo de los resultados
+    rickMortyResults.innerHTML = "";
+
+    //usamos comillas invertidas para embebir HTML directamente
+    rickMortyResults.innerHTML = `
+        <h2>${results.name}</h2>
+        <img src="${results.image}" alt="${results.name}">
+        <p>Género:${results.gender}</p>
+        <p>Capítulos en los que aparece: ${results.episode.length}</p>
+    `
 }
 
-buscaPersonaje(2); // Busca al personaje con ID 1
+
+function buscaPersonaje(nameCharacter) {
+    const urlCharacter = `http://rickandmortyapi.com/api/character/?name=${nameCharacter}`
+    fetch(urlCharacter)
+
+    .then((response) => {
+        if (response.status != 200) {
+            throw new Error("Personaje no encontrado");
+        }
+        return response.json();
+    })
+        .then((data) => {
+            displayRickMortyResults(data.results[0])
+            
+        })
+        .catch(error => {
+            rickMortyResults.textContent = error.message
+        })
+}
+
+//Añadimos un evento al botón de búsqueda para poder hacer la petición a la api
+searchButton.addEventListener("click", () => {
+    //Obtengo el nombre del pokemon del input
+    //trim(): Validación que remueve los espacios sobrantes antes y al final del texto
+    //toLowerCase(): Validación para hacer la búsqueda en minúsculas independientemente de cómo lo escriba el usuario
+    const nameCharacter = rickMortyNameInput.value.trim(); 
+    if (nameCharacter){
+        //si el usuario coloca por lo menos un caracteren el input, realizo la petición a la api
+        rickMortyNameInput.value = ""; // Limpio el input para que el usuario pueda buscar otro pokemon en el futuro
+        buscaPersonaje(nameCharacter) //función que hace la solicitud a la api
+    }else{
+        //sino, mando una advertencia
+        rickMortyResults.textContent = "Por favor, ingresa un nombre de de la serie"
+    }
+
+    console.log(nameCharacter);
+});
+
+
